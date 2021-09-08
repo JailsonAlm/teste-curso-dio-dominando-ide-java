3adc21c4ffc6142da3dacd8d2148e79be8ad381c
JailsonAlm <jailson.07almeida@gmail.com>
2021-09-07 21:24:04 -0300

    first commit

---
+++ src/br/com/dio/SegundoPrograma.java
@@ -0,0 +1,26 @@
+package br.com.dio;
+
+import br.com.dio.model.gato;
+
+public class SegundoPrograma {
+
+	public static void main(String[] args) {
+		gato gato = new gato();
+		Livros Livros = new Livros();
+		
+		System.out.println(gato);
+		System.out.println(Livros);
+		/*int a = 2;
+		int b = 3;
+		System.out.println("Hello Word! "+(a+b));
+		// TODO Auto-generated method stub*/
+
+	}
+
+}
+
+class Livros {
+	private String nome;
+	private String npag;
+	
+}
